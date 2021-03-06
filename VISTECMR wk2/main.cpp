3fcaca73e944c788983c47f9b68ee6f275bfb7d6
Jasper <j.bonefaas@student.avans.nl>
2021-03-06 17:39:10 +0100

    Added brick shader

--- VISTECMR wk2/main.cpp
+++ VISTECMR wk2/main.cpp
@@ -43,6 +43,7 @@ void init()
 	glClearColor(1, 0.7f, 0.3f, 1.0f);
 
 	currentShader = 0;
+	shaders.push_back(new Shader("assets/shaders/brick"));
 	shaders.push_back(new Shader("assets/shaders/toonshading"));
 	shaders.push_back(new Shader("assets/shaders/simple"));
 	shaders.push_back(new Shader("assets/shaders/multitex"));
